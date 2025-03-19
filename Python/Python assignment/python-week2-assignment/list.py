8acafd03efde08d106da2d5caa4dae2201cc256e
Sifa001 <sifavivz@gmail.com>
2025-03-19 20:47:08 +0300

    assignment

---
+++ Python/Python assignment/python-week2-assignment/list.py
@@ -0,0 +1,28 @@
+#creating an empty list
+my_list = []
+
+#appending elements to the list
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+#inserting a value to the list
+my_list.insert(1,15)
+
+#extending the list
+my_list.extend([50,60,70])
+
+#removing the last element on the list
+my_list.pop()
+
+#sorting the list in ascending order
+my_list.sort()
+
+#finding and printing index of 30 in the list
+indexOf30 = my_list.index(30)
+print(indexOf30)
+
+
+
+
